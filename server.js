d193f56bcd449fccbec0f7c56940cb817781b4d3
GabrielCaspirro <gabriel.gcd08@gmail.com>
2025-08-19 09:04:27 -0300

    get e post com mongoDB

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 import express from "express";
 
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
+
 const app = express();
 
 app.use(express.json());
 
-const usuarios = [];
+app.get("/usuarios", async (req, res) => {
 
-app.get("/usuarios", (req, res) => {
+    const usuarios = await prisma.usuario.findMany();
+
     res.status(200).json(usuarios);
 });
 
-app.post("/cadastro", (req, res) => {
-    const usuario = req.body;
-    usuarios.push(usuario);
-    res.status(201).json(usuario);
+app.post("/cadastro", async (req, res) => {
+
+    await prisma.usuario.create({
+        data: {
+            email: req.body.email,
+            nome: req.body.nome,
+            idade: req.body.idade
+        }
+    });
+
+    res.status(201).json(req.body);
 });
 
 app.listen(3000, () => {
